cf065339e2c3045498bba4e64e1aafd98bc37328
Adrián <123007421+Kirk810@users.noreply.github.com>
2024-06-14 21:16:12 +0200

    Create Home.jsx

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <section id="home">
+      <h2>Welcome to My Portfolio</h2>
+      <p>
+        Hi, I am Adrián Soto Maeso, a web developer. Welcome to my personal
+        portfolio website!
+      </p>
+    </section>
+  );
+};
+
+export default Home;
