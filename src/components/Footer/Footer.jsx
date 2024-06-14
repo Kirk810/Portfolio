b90b0e9e6aff665351ad143a3d52a047044464af
Adrián <123007421+Kirk810@users.noreply.github.com>
2024-06-14 21:15:50 +0200

    Create Footer.jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="Footer">
+        <p>&copy; 2024 My Portfolio. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
