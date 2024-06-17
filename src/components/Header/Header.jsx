ac2d4bd11d316b2f92198afdc08e48d6cc26928a
Adrián <123007421+Kirk810@users.noreply.github.com>
2024-06-17 20:52:04 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -22,6 +22,26 @@ const Header = () => {
               <a href="#contact">CONTACT</a>
             </li>
           </ul>
+          <div className="link">
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/adriansotomaeso/"
+            >
+              <img
+                src="https://res.cloudinary.com/dvdoak5et/image/upload/v1718648641/Portfolio/svg_xml_base64_PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZGF0YS1zdXBwb3J0ZWQtZHBzPSIyNHgyNCIgZmlsbD0iY3VycmVudENvbG9yIiBjbGFzcz0ibWVyY2Fkby1tYXRjaCIgd2lkdGg9IjI0IiBoZWlnaHQ9IjI0_bh4zia.svg"
+                alt="Linkedin"
+              />
+            </a>
+            <a
+              target="_blank"
+              href="https://github.com/Kirk810?tab=repositories"
+            >
+              <img
+                src="https://res.cloudinary.com/dvdoak5et/image/upload/v1718648651/Portfolio/svg_xml_base64_PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGFyaWEtaGlkZGVuPSJ0cnVlIiBoZWlnaHQ9IjI0IiB2aWV3Qm94PSIwIDAgMTYgMTYiIHZlcnNpb249IjEuMSIgd2lkdGg9IjI0IiBkYXRhLXZpZXctY29tcG9uZW50PSJ0cnVlIiBjbGFzcz0i_llrekl.svg"
+                alt="Github"
+              />
+            </a>
+          </div>
         </nav>
       </div>
     </header>
