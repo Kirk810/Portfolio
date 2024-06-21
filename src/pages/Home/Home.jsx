31aa2ba94985ee92c33629eda8ee41f0daebdbbf
Adrián <123007421+Kirk810@users.noreply.github.com>
2024-06-21 13:33:37 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <section className="Home" id="home">
+    <section className="home" id="home">
       <h1>Welcome to My Portfolio</h1>
       <p>
         Hi, I am Adrián Soto Maeso, a web developer. Welcome to my personal
