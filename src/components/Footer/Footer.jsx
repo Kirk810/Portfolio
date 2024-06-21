1ea88da39677f37caf44d966e9363e998533b461
Adrián <123007421+Kirk810@users.noreply.github.com>
2024-06-21 13:33:27 +0200

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer>
-      <div className="Footer">
+      <div className="footer">
         <p>&copy; 2024 My Portfolio. All rights reserved.</p>
       </div>
     </footer>
